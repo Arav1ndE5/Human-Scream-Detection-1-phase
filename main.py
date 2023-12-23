d657978cd491845168df97784e11ca84dfdd6f68
Arav1ndE5 <aravind.es@gmail.com>
2023-12-23 17:37:05 +0530

    check

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-# %%
-import os
+
+#  HUMAN SCREAM DETECTION USING MACHINE LEARNING
+
+# Importing the libraries
 import numpy as np
 import librosa
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-import tensorflow as tf
 from tensorflow.keras import layers, models, callbacks
 from tensorflow.keras.utils import to_categorical
 import sounddevice as sd
+import matplotlib.pyplot as plt
+import soundfile as sf
 from scipy.io.wavfile import write
 from IPython.display import display, Audio
 
-# %%
+
 # Function to extract features from audio files using librosa
 def extract_features(file_path, mfcc=True, chroma=True, mel=True):
     y, sr = librosa.load(file_path, mono=True)
@@ -28,18 +31,19 @@ def extract_features(file_path, mfcc=True, chroma=True, mel=True):
     return features
 
 
-# %%
+
 # Example to train the model, evaluate, and test with recorded audio
 scream_folder = "Assets/positive"
 non_scream_folder = "Assets/negative"
 
-# %%
+
 # Function to load and preprocess the dataset
 def load_and_preprocess_data(scream_folder, non_scream_folder):
     features = []
     labels = []
 
     # Load scream data
+    import os
     for filename in os.listdir(scream_folder):
         path = os.path.join(scream_folder, filename)
         feature = extract_features(path)
@@ -68,7 +72,7 @@ def load_and_preprocess_data(scream_folder, non_scream_folder):
 
     return X_train, X_test, y_train, y_test, le
 
-# %%
+
 # Define the model
 def create_model(input_shape):
     model = models.Sequential()
@@ -84,14 +88,14 @@ def create_model(input_shape):
 
     return model
 
-# %%
+
 # Evaluate the model
 def evaluate_model(model, X_test, y_test):
     loss, accuracy = model.evaluate(X_test, y_test)
     print(f'Test loss: {loss:.4f}')
     print(f'Test accuracy: {accuracy:.4f}')
 
-# %%
+
 # Training the model
 def train_model(X_train, y_train, X_test, y_test, epochs=150, batch_size=32):
     input_shape = X_train.shape[1]
@@ -105,19 +109,42 @@ def train_model(X_train, y_train, X_test, y_test, epochs=150, batch_size=32):
                         validation_data=(X_test, y_test))
     # history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size,
     #                     validation_data=(X_test, y_test), callbacks=[early_stopping])
+    
+    # Plot training & validation accuracy values
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('Model accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
+
+    # Plot training & validation loss values
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
 
     return model, history
 X_train, X_test, y_train, y_test, label_encoder = load_and_preprocess_data(scream_folder, non_scream_folder)
 model, history = train_model(X_train, y_train, X_test, y_test)
 evaluate_model(model, X_test, y_test)
 
-# %%
+
+
+
+
+
 # Function to predict whether an audio file contains a scream or not
 def predict_audio(file_path, model, label_encoder):
     
     feature = extract_features(file_path)
     feature = np.array(feature).reshape(1, -1)
     prediction = model.predict(feature)
+    display(Audio(file_path))
 
     if len(prediction) == 0:
         return "Unknown"
@@ -125,7 +152,7 @@ def predict_audio(file_path, model, label_encoder):
     predicted_label = label_encoder.inverse_transform([np.argmax(prediction)])
     return predicted_label[0]
 
-# %%
+
 # Example to test an audio file
 
 # Test : 1
@@ -140,7 +167,7 @@ if predicted_label == 1:
 else:
   print("No alert required")
 
-# %%
+
 # Example to test an audio file
 
 # Test : 2
@@ -155,7 +182,7 @@ if predicted_label == 1:
 else:
   print("No alert required")
 
-# %%
+
 # Function to record audio and save it to testing
 def record_audio(output_file, duration=10, sample_rate=44100):
     print(f"Recording... Please speak for {duration} seconds.")
@@ -169,7 +196,7 @@ def record_audio(output_file, duration=10, sample_rate=44100):
 
     print(f"Recording saved to {output_file}.")
 
-# %%
+
 # Example to record audio and test the prediction
 output_file_path = "Assets/testing/recorded_audio.wav"
 record_audio(output_file_path)
@@ -177,6 +204,7 @@ record_audio(output_file_path)
 # Predict the label for the recorded audio
 predicted_label = predict_audio(output_file_path, model, label_encoder)
 print(f"The predicted label for the recorded audio is: {predicted_label}")
+print()
 
 if predicted_label == 1:
     print("Nearby Officer is alerted")
